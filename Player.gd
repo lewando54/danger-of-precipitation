5d0dcfdea4768c3828f941539ecd630531619c2d
Jakub Lewandowski <lewando54@gmail.com>
2023-06-08 13:28:48 +0200

    Finished basic shooting and movement

--- Player.gd
+++ Player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
 var curr_weapon
-var weapons = ["Basic"]
+var curr_weapon_instance
+var weapons = ["Basic", "Laser"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x = get_viewport_rect().size.x / 2
 	position.y = get_viewport_rect().size.y / 2
 	curr_weapon = load("res://Weapons/"+weapons[0]+"/weapon.tscn")
-	add_child(curr_weapon.instantiate())
+	curr_weapon_instance = curr_weapon.instantiate()
+	add_child(curr_weapon_instance)
 
 var speed = 1200
 var acceleration = 10
@@ -26,3 +28,6 @@ func _physics_process(delta):
 	target_velocity = target_velocity.normalized() * speed
 	velocity = velocity.lerp(target_velocity, acceleration * delta)
 	move_and_slide()
+	
+	if Input.is_action_pressed("shoot"):
+		SignalBus.emit_signal("on_player_shoot")
--- Weapons/Basic/weapon.gd
+++ Weapons/Basic/weapon.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 var scale_y
 var m_scale_y
+@export var bullet_speed = 1
+@export var shoot_speed = 2
+@export var bullet : PackedScene
 
+var can_shoot = true
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	SignalBus.on_player_shoot.connect(_on_player_shot)
 	scale_y = $AnimatedSprite2D.scale.y
 	m_scale_y = -scale_y
 
@@ -22,3 +28,7 @@ func _process(delta):
 	else:
 		$AnimatedSprite2D.scale.y = scale_y
 
+func _on_player_shot():
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.global_transform = $BulletSpawner.global_transform
+	get_tree().root.add_child(bullet_instance)
